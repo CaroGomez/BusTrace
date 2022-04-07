f7df7f67513f9fab26f3df60ef5a8f3688e59fe8
CaroGomez <astridc.diaz@udea.edu.co>
2022-04-07 16:00:37 -0500

    continuación para ruta 1

--- app/src/main/java/co/edu/udea/compumovil/gr05_20212/bustrace/route_details.kt
+++ app/src/main/java/co/edu/udea/compumovil/gr05_20212/bustrace/route_details.kt
@@ -2,10 +2,15 @@ package co.edu.udea.compumovil.gr05_20212.bustrace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class route_details : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_route_details)
+        database = Firebase.database.reference
     }
 }
